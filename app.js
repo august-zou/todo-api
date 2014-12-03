0f8315e5a2c03953c793400abb2dffdee5914e63
august-zou <zjzouxu@gmail.com>
2014-12-03 13:44:59 +0800

    add api

---
+++ app.js
@@ -0,0 +1,44 @@
+/*!
+ * koa-todo - app.js
+ * Copyright(c) 2014 dead_horse <dead_horse@qq.com>
+ * MIT Licensed
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var middlewares = require('koa-middlewares');
+var routes = require('./routes');
+var config = require('./config');
+var path = require('path');
+var http = require('http');
+var koa = require('koa');
+
+var app = koa();
+
+/**
+ * response time header
+ */
+app.use(middlewares.rt());
+
+app.use(middlewares.bodyParser());
+
+if (config.debug && process.env.NODE_ENV !== 'test') {
+  app.use(middlewares.logger());
+}
+
+/**
+ * router
+ */
+app.use(middlewares.router(app));
+routes(app);
+
+app = module.exports = http.createServer(app.callback());
+
+if (!module.parent) {
+  app.listen(config.port);
+  console.log('$ open http://127.0.0.1:' + config.port);
+}
